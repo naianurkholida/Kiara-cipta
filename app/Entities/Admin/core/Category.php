99520703a649127a0ef4975b4c7668f223b799c8
dziwww <adzibilal02@gmail.com>
2020-02-28 00:19:36 +0700

    detail item

--- app/Entities/Admin/core/Category.php
+++ app/Entities/Admin/core/Category.php
@@ -11,6 +11,6 @@ class Category extends Model
 
     public function getGallery()
     {
-        return $this->hasMany(Gallery::class, 'id_category', 'id');
+        return $this->hasMany(Gallery::class, 'id_category', 'id')->where('deleted_at', NULL);
     }
 }
