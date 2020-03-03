d01af956f05f65cf4ab5a28b1fa72ad685f59f91
Agus Setiawan <luciversetiawan110@gmail.com>
2020-03-03 19:38:58 +0700

    error

--- resources/views/frontend/check_point.blade.php
+++ resources/views/frontend/check_point.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div class="container">
         <div class="container">
-            <iframe style="border:0px #ffffff none;" width="100%" height="400" src="http://103.11.134.45:1717/apex/f?p=889:1" scrolling="no" frameborder="1" allowfullscreen=""></iframe>
+            <iframe style="border:0px #ffffff none;" width="100%" height="1000px;" src="http://103.11.134.45:1717/apex/f?p=889:1" scrolling="no" frameborder="1" allowfullscreen=""></iframe>
             <!-- <iframe src="http://localhost:8000/derma-express/checkpoint" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe> -->
         </div>
     </div>
