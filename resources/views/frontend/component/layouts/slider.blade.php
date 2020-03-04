8e57171668a7d28521d436e25b72f285c1dcc5d1
dziwww <adzibilal02@gmail.com>
2020-03-04 18:15:05 +0700

    slider

--- resources/views/frontend/component/layouts/slider.blade.php
+++ resources/views/frontend/component/layouts/slider.blade.php
@@ -1,31 +1,16 @@
-<!-- Slider
-		============================================= -->
-<section id="slider" style="height:450px !importnant;background-color:#f9f9f9;"
-    class="slider-element slider-parallax swiper_wrapper full-screen clearfix" data-autoplay="7000" data-speed="650"
-    data-loop="true">
-
-    <div class="slider-parallax-inner" style="height:450px !importnant;">
-
-        <div class="swiper-container swiper-parent">
-            <div class="swiper-wrapper">
-
-                @foreach(Helper::slider() as $row)
-                <div class="swiper-slide"
-                    style="background-image: url( {{ $row->getFirstMediaUrl('slider') }} ); background-position: center top;">
-                </div>
-                @endforeach
-
+ <section id="slider" class="slider-element boxed-slider" style="padding-top:0px !important;">
+    <div class="fslider" data-easing="easeInQuad">
+      <div class="flexslider">
+        <div class="slider-wrap">
+          @foreach(Helper::slider() as $row)
+            <div class="slide" data-thumb="{{ $row->getFirstMediaUrl('slider') }}">
+              <a href="#">
+                <div class="img-slider" style="background-image: url({{ $row->getFirstMediaUrl('slider') }});height:500px;width:100%;"></div>
+              </a>
             </div>
-            <div class="slider-arrow-left"><i class="icon-angle-left"></i></div>
-            <div class="slider-arrow-right"><i class="icon-angle-right"></i></div>
-            <div class="slide-number">
-                <div class="slide-number-current"></div><span>/</span>
-                <div class="slide-number-total"></div>
-            </div>
-            <div class="swiper-pagination"></div>
+          @endforeach
+          
         </div>
-
+      </div>
     </div>
-
 </section>
-<!-- #Slider End --> 
