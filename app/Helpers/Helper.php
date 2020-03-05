5b18970d41e7cc1da1d24fa355bdb98fb209fba4
Agus Setiawan <luciversetiawan110@gmail.com>
2020-03-05 15:36:04 +0700

    replace

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -100,7 +100,7 @@ class Helper
 	
 	public static function removeTags($tags)
 	{
-		$data = substr(strip_tags($tags), 0, 140) . '...';
+		$data = str_replace('&nbsp;','',substr(strip_tags($tags), 0, 140)) . '...';
 
 		return $data;
 	}
