1f30d9a9ee9a6eca8ad74ef7b3088d01cfff1048
dziwww <adzibilal02@gmail.com>
2020-03-05 16:32:34 +0700

    slider

--- resources/views/frontend/component/layouts/slider.blade.php
+++ resources/views/frontend/component/layouts/slider.blade.php
@@ -1,11 +1,11 @@
-<section id="slider" class="slider-element boxed-slider" style="padding-top:0px !important;background-size: contain;background-repeat: no-repeat;background-position: center;height: 500px;width: 100%;">
+<section id="slider" class="slider-element boxed-slider" style="padding-top:0px !important;">
     <div class="fslider" data-easing="easeInQuad">
       <div class="flexslider">
         <div class="slider-wrap">
           @foreach(Helper::slider() as $row)
             <div class="slide" data-thumb="{{ $row->getFirstMediaUrl('slider') }}">
               <a href="#">
-                <div class="img-slider" style="background-image: url({{ $row->getFirstMediaUrl('slider') }});height:500px;width:100%;"></div>
+                <div class="img-slider" style="background-image: url({{ $row->getFirstMediaUrl('slider') }});height:500px;width:100%;background-size: contain;background-repeat: no-repeat;background-position: center;height: 500px;width: 100%;"></div>
               </a>
             </div>
           @endforeach
