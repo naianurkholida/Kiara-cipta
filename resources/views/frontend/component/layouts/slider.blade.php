9cc08ffe7a90f6e19ddebe06d777cdb98f4edf8a
dziwww <adzibilal02@gmail.com>
2020-03-05 16:07:28 +0700

    slider flex

--- resources/views/frontend/component/layouts/slider.blade.php
+++ resources/views/frontend/component/layouts/slider.blade.php
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
- <section id="slider" class="slider-element boxed-slider" style="padding-top:0px !important;">
+<section id="slider" class="slider-element boxed-slider" style="padding-top:0px !important;">
     <div class="fslider" data-easing="easeInQuad">
       <div class="flexslider">
         <div class="slider-wrap">
@@ -15,24 +14,7 @@
               <div class="img-slider" style="background-image: url({{ asset('assets/images/blog/full/10.jpg') }});height:500px;width:100%;"></div>
             </a>
           </div>
-=======
-
-<section id="slider" style="height:450px !importnant;background-color:#f9f9f9;"
-    class="slider-element slider-parallax swiper_wrapper full-screen clearfix" data-autoplay="7000" data-speed="650"
-    data-loop="true">
-    <div class="slider-parallax-inner" style="height:450px !importnant;">
-
-        <div class="swiper-container swiper-parent">
-            <div class="swiper-wrapper">
-
-                @foreach(Helper::slider() as $row)
-                <div class="swiper-slide"
-                    style="background-image: url( {{ $row->getFirstMediaUrl('slider') }} ); background-position: center top;">
-                </div>
-                @endforeach
-          
->>>>>>> ef585836796da9f751c92b574286816530840b5b
         </div>
       </div>
-    </div>
+    </div>    
 </section>
