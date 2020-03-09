fe53808f5b2e2427c4e626ff61be2cfaf1959411
Adzi Bilal Maulana H <adzibilal02@gmail.com>
2020-03-09 20:05:23 +0700

    responsive detail Product

--- resources/views/frontend/products-detail.blade.php
+++ resources/views/frontend/products-detail.blade.php
@@ -1,5 +1,11 @@
 @extends('frontend.component.master')
 
+@section('css')
+    <style>
+
+    </style>
+@endsection
+
 @section('content')
 <div class="section nobg nobottommargin clearfix" style="margin-top: 0;">
     <div class="container clearfix">
@@ -7,10 +13,24 @@
             <h3 class="nott ls0">Products</h3>
         </div>
     </div>
-    <div class="container">
-        <div class="container">
+    <div class="container" id="container_luar">
+        <div class="container" id="container_dalem">
             @include('frontend.component.layouts.item-detail', ['related' => 'getProdukLanguage', 'column' => 'deskripsi','image' => 'produk'])
         </div>
     </div>
 </div>
+@endsection
+
+@section('js')
+    <script>
+        
+        if (window.matchMedia('(max-width: 425px)'))
+        {
+            $( "#container_dalem" ).removeClass("container");
+            $( "#container_luar" ).removeClass("container").addClass("container-fluid");
+        } else {
+            $( "#container_dalem" ).addClass("container");
+            $( "#container_luar" ).removeClass("container-fluid").addClass("container");
+        }
+    </script>
 @endsection
