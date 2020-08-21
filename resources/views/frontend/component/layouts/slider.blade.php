e8c3c5d4eeab3f9acded969da0fbc97b5c40dfa3
Adzi Bilal Maulana H <adzibilal02@gmail.com>
2020-08-21 16:19:56 +0700

    slider jadi full

--- resources/views/frontend/component/layouts/slider.blade.php
+++ resources/views/frontend/component/layouts/slider.blade.php
@@ -1,8 +1,9 @@
 <div class="slider-container">
   <div id="slider-owl" class="owl-carousel" style="display: inline;">
     @foreach(Helper::slider() as $key=>$row)
-      <div class="item" data-hash="{{$key+1}}">
-        <div class="img-slider" style="background-image: url({{ $row->getFirstMediaUrl('slider') }});height:700px;width:100%;background-size: contain;background-repeat: no-repeat;background-position: center;height: 700px;width: 100%;"></div>
+      <div class="item" data-hash="{{$key+1}}" style="width: 100%;min-height:700px;">
+        {{-- <div class="img-slider" style="background-image: url({{ $row->getFirstMediaUrl('slider') }});height:700px;width:100%;background-size: contain;background-repeat: no-repeat;background-position: center;height: 700px;width: 100%;"></div> --}}
+        <img src="background-image: url({{ $row->getFirstMediaUrl('slider') }});" alt="" style="width:100%;">
       </div>
     @endforeach
   </div>
