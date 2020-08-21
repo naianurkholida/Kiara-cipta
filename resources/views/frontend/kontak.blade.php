e536d0947768e25baa0622f4c9226d1e5762f4ba
Agus Setiawan <luciversetiawan110@gmail.com>
2020-08-21 17:53:49 +0700

    maintenance agustus

--- resources/views/frontend/kontak.blade.php
+++ resources/views/frontend/kontak.blade.php
@@ -12,13 +12,13 @@
                     <form method="POST" action="{{ route('inbox.post') }}">
                         @csrf
                         <div class="form-group">
-                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nama Lengkap" name="name">
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nama Lengkap" name="name" style="border-radius: 10px;">
                         </div>
                         <div class="form-group">
-                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Masukan alamat email anda" name="email">
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Masukan alamat email" name="email" style="border-radius: 10px;">
                         </div>
                         <div class="form-group">
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="inbox"></textarea>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="inbox" style="border-radius: 10px;"></textarea>
                         </div>
 
                         <button type="submit" class="btn btn-primary">Kirim</button>
