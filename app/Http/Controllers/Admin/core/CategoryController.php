7e918a489340627fa2755623c5946363593be329
Agus Setiawan <luciversetiawan110@gmail.com>
2020-08-25 17:09:12 +0700

    profile

--- app/Http/Controllers/Admin/core/CategoryController.php
+++ app/Http/Controllers/Admin/core/CategoryController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Entities\Admin\core\Category;
 use App\Entities\Admin\core\MenuAccess;
 use App\Entities\Admin\core\Parameter;
-use App\Entities\Admin\core\Gambar;
 use App\Http\Controllers\Controller;
 use App\Entities\Admin\core\MenuFrontPage;
 use App\Entities\Admin\core\MenuFrontPageLanguage;
@@ -55,13 +54,13 @@ class CategoryController extends Controller
     public function index()
     {
     	$top_bar = $this->top_bar();
-        if(Session::get('role_id') == '1'){
-            $categories = Category::where('id_parent', '0')->get();
-        }else{
-            $categories = Category::where('id_parent', '0')
-                        ->where('is_created', 1)
-                        ->get();
-        }
+    	if(Session::get('role_id') == '1'){
+    		$categories = Category::where('id_parent', '0')->get();
+    	}else{
+    		$categories = Category::where('id_parent', '0')
+    		->where('is_created', 1)
+    		->get();
+    	}
     	$validasi = MenuAccess::select('*')
     	->leftjoin('menus', 'menus.id', '=', 'menu_access.menu_id')
     	->where('role_id', \Session::get('role_id'))
@@ -97,25 +96,25 @@ class CategoryController extends Controller
     	$category->id_language = 1;
     	$category->category = $request->kategori;
     	$category->order_num = $request->order_num;
-        $category->is_created = Session::get('id');
-        $category->save();
-        
-        $menu = new MenuFrontPage;
-        $menu->id_category = 8;
-        $menu->id_sub_menu = 3;
-        $menu->jenis = 'Page';
-        $menu->sort_order = 1;
-        $menu->url = strtolower($request->kategori);
-        $menu->is_created = \Session::get('id');
-        $menu->save();
+    	$category->is_created = Session::get('id');
+    	$category->save();
 
-        for($i = 0; $i < 2; $i++){
-            $menulang = new MenuFrontPageLanguage;
-            $menulang->id_menu_front_page = $menu->id;
-            $menulang->id_language = 1;
-            $menulang->judul_menu = $request->kategori;
-            $menulang->save();
-        }
+    	$menu = new MenuFrontPage;
+    	$menu->id_category = 8;
+    	$menu->id_sub_menu = 3;
+    	$menu->jenis = 'Page';
+    	$menu->sort_order = 1;
+    	$menu->url = strtolower($request->kategori);
+    	$menu->is_created = \Session::get('id');
+    	$menu->save();
+
+    	for($i = 0; $i < 2; $i++){
+    		$menulang = new MenuFrontPageLanguage;
+    		$menulang->id_menu_front_page = $menu->id;
+    		$menulang->id_language = 1;
+    		$menulang->judul_menu = $request->kategori;
+    		$menulang->save();
+    	}
 
     	return redirect('category')->with('success', 'Data Berhasil di Simpan');
     }
@@ -146,7 +145,7 @@ class CategoryController extends Controller
     	$category = Category::find($id);
     	// $id_cek  = Parameter::where('key', 'kategori_program')->first();
     	// $cek_kat = Category::where('id_parent', $id_cek->value)->where('id', $category->id)->first();
-        $cek_kat = '';
+    	$cek_kat = '';
     	return view('admin.core.category.edit', compact('top_bar', 'category', 'cek_kat'));
     }
 
@@ -160,46 +159,46 @@ class CategoryController extends Controller
     public function update(Request $request, $id)
     {
     	$category = Category::find($id);
-        if($request->file('gambar') != null){
-           File::delete('admin/assets/media/icon-kategori/'.$category->gambar);
-       }
-       
-       $category->category = $request->kategori;
-       $category->order_num = $request->order_num;
-       $category->is_created = Session::get('id');
-       $category->save();
+    	if($request->file('gambar') != null){
+    		File::delete('admin/assets/media/icon-kategori/'.$category->gambar);
+    	}
+
+    	$category->category = $request->kategori;
+    	$category->order_num = $request->order_num;
+    	$category->is_created = Session::get('id');
+    	$category->save();
 
        // $id_cek  = Parameter::where('key', 'kategori_program')->first();
        // $cek_kat = Category::where('id_parent', $id_cek->value)->where('id', $category->id)->first();
 
-       $cek_kat = '';
+    	$cek_kat = '';
 
-       if($cek_kat != null && $request->file('gambar') != null){
-          $gambar = Gambar::where('id_relasi', $category->id)->first();
-          $gambar->id_relasi = $category->id;
-          $gambar->deskripsi = $request->deskripsi;
-    		#upload foto to database
-          $file = $request->file('gambar');
+    	if($cek_kat != null && $request->file('gambar') != null){
+    		// $gambar = Gambar::where('id_relasi', $category->id)->first();
+    		// $gambar->id_relasi = $category->id;
+    		// $gambar->deskripsi = $request->deskripsi;
+    		// #upload foto to database
+    		// $file = $request->file('gambar');
 
-	    	#JIKA FOLDERNYA BELUM ADA
-          if (!File::isDirectory($this->path)) {
-	    		#MAKA FOLDER TERSEBUT AKAN DIBUAT
-             File::makeDirectory($this->path);
-         }
+	    	// #JIKA FOLDERNYA BELUM ADA
+    		// if (!File::isDirectory($this->path)) {
+	    	// 	#MAKA FOLDER TERSEBUT AKAN DIBUAT
+    		// 	File::makeDirectory($this->path);
+    		// }
 
-	    	#MEMBUAT NAME FILE DARI GABUNGAN TIMESTAMP DAN UNIQID()
-         $fileName = 'Icon'.'_'.uniqid().'.'.$file->getClientOriginalExtension();
+	    	// #MEMBUAT NAME FILE DARI GABUNGAN TIMESTAMP DAN UNIQID()
+    		// $fileName = 'Icon'.'_'.uniqid().'.'.$file->getClientOriginalExtension();
 
-	    	#UPLOAD ORIGINAN FILE (BELUM DIUBAH DIMENSINYA)
-         Image::make($file)->save($this->path.'/'. $fileName);
+	    	// #UPLOAD ORIGINAN FILE (BELUM DIUBAH DIMENSINYA)
+    		// Image::make($file)->save($this->path.'/'. $fileName);
 
-	    	#SIMPAN DATA IMAGE YANG TELAH DI-UPLOAD
-         $gambar->gambar = $fileName;
-         $gambar->save();
-     }
+	    	// #SIMPAN DATA IMAGE YANG TELAH DI-UPLOAD
+    		// $gambar->gambar = $fileName;
+    		// $gambar->save();
+    	}
 
-     return redirect('category')->with('info', 'Data Berhasil di Update');
- }
+    	return redirect('category')->with('info', 'Data Berhasil di Update');
+    }
 
     /**
      * Remove the specified resource from storage.
@@ -209,20 +208,20 @@ class CategoryController extends Controller
      */
     public function delete($id)
     {
-    	$category = Gambar::where('id_relasi', $id)->first();
-    	if($category != null){
-    		File::delete('admin/assets/media/icon-kategori/'.$category->gambar);
-    	}
+    	// $category = Gambar::where('id_relasi', $id)->first();
+    	// if($category != null){
+    	// 	File::delete('admin/assets/media/icon-kategori/'.$category->gambar);
+    	// }
 
-    	Gambar::where('id_relasi', $id)->delete();
-        $dataCat = Category::where('id', $id);
+    	// Gambar::where('id_relasi', $id)->delete();
+    	$dataCat = Category::where('id', $id);
 
-        $language = MenuFrontPageLanguage::where('judul_menu', $dataCat->category);
-        $languageFirst = MenuFrontPage::where('id', $language[0]->id_menu_front_page)->delete();
+    	$language = MenuFrontPageLanguage::where('judul_menu', $dataCat->category);
+    	$languageFirst = MenuFrontPage::where('id', $language[0]->id_menu_front_page)->delete();
 
-        $dataCat->delete();
-        $language->delete();
-        
+    	$dataCat->delete();
+    	$language->delete();
+
     	return redirect('category')->with('danger', 'Data Berhasil di Hapus');
     }
 }
