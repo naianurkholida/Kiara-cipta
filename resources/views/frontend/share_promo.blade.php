7d3bc9d86c98bfc6742e5e6df97b60e7cf9752cd
Agus Setiawan <luciversetiawan110@gmail.com>
2020-09-02 14:03:23 +0700

    maintenance_agustus

--- resources/views/frontend/share_promo.blade.php
+++ resources/views/frontend/share_promo.blade.php
@@ -2,11 +2,7 @@
 
 @section('content')
 <div class="section nobg nobottommargin clearfix" style="margin-top: 0;">
-    <div class="container clearfix">
-        <div class="heading-block center noborder" data-heading="O">
-            <h3 class="nott ls0">Promo</h3>
-        </div>
-    </div>
+ 
     <div class="container">
         <div class="container">
             <div class="row">
