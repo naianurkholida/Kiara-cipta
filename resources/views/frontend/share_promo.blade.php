fd2560604b05f61c061dd4533efd608889b06173
Agus Setiawan <luciversetiawan110@gmail.com>
2020-09-09 17:07:41 +0700

    promo

--- resources/views/frontend/share_promo.blade.php
+++ resources/views/frontend/share_promo.blade.php
@@ -4,12 +4,24 @@
 <div class="section nobg nobottommargin clearfix" style="margin-top: 0;">
  
     <div class="container">
-        <div class="container">
+        <div class="container" id="container_dalem">
             <div class="row">
-                 <iframe style="border:0px #ffffff none;" width="100%" height="1150px;" src="{{url('')}}/promosi/{{$seo}}" scrolling="no" frameborder="1" allowfullscreen=""></iframe>
+                <iframe style="border:0px #ffffff none;" width="100%" height="1000px;" src="{{url('')}}/promosi/{{$seo}}" scrolling="no" frameborder="1" allowfullscreen=""></iframe>
             </div>
         </div>
     </div>
 
 </div>
+@endsection
+
+@section('js')
+    <script>
+        
+        if (window.matchMedia('(max-width: 425px)'))
+        {
+            $( "#container_dalem" ).removeClass("container");
+        } else {
+            $( "#container_dalem" ).addClass("container");
+        }
+    </script>
 @endsection
