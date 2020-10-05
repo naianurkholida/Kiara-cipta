4f18633a02ee0d5d9226ac631aa2c04a499b21c6
Agus Setiawan <luciversetiawan110@gmail.com>
2020-10-05 19:19:21 +0700

    inbox

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -58,9 +58,6 @@
                             <table cellpadding="0" cellspacing="0" border="0" width="88%" style="width: 88% !important; min-width: 88%; max-width: 88%;">
                                 <tr>
                                     <td align="left" valign="top">
-                                        <font face="'Source Sans Pro', sans-serif" color="#1a1a1a" style="font-size: 52px; line-height: 60px; font-weight: 300; letter-spacing: -1.5px;">
-                                            <span style="font-family: 'Source Sans Pro', Arial, Tahoma, Geneva, sans-serif; color: #1a1a1a; font-size: 52px; line-height: 60px; font-weight: 300; letter-spacing: -1.5px;">Derma Express</span>
-                                        </font>
                                         <font face="'Source Sans Pro', sans-serif" color="#585858" style="font-size: 24px; line-height: 32px;">
                                             <span style="font-family: 'Source Sans Pro', Arial, Tahoma, Geneva, sans-serif; color: #585858; font-size: 24px; line-height: 32px;">
                                                {{ $pesan }}
