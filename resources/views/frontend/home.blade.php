fca693b3c5418758be18c877de1863aeff8296f9
Agus Setiawan <luciversetiawan110@gmail.com>
2020-10-06 13:55:59 +0700

    hide jurnal

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -9,8 +9,6 @@
 
 @include('frontend.component.layouts.bestseller')
 
-<!-- @include('frontend.component.layouts.youtube') -->
-
 <div class="pop-container">
     <div class="box-pop">
         <div class="close-pop">X</div>
