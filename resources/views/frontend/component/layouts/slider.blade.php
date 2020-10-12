1bf9b433d92b2b9af1bb2bf72bb7a91e328ef2e6
Adzi Bilal Maulana H <adzibilal02@gmail.com>
2020-10-12 19:00:11 +0700

    test slider

--- resources/views/frontend/component/layouts/slider.blade.php
+++ resources/views/frontend/component/layouts/slider.blade.php
@@ -1,29 +1,21 @@
 <div class="slider-container">
-  <div id="slider-owl" class="owl-carousel owl-theme">
-    <?php $code_warna = ''; ?>
-    @foreach(Helper::slider() as $key=>$row)
-    <?php 
-    if($row->code_warna != null){ 
-      $code_warna = $row->code_warna; 
-    }else{ 
-      $code_warna = "#dedede;";
-    }?>
-    <div class="item" data-hash="{{$key+1}}">
-      <div class="img-slider" style="background-image: url({{ $row->getFirstMediaUrl('slider') }}); 
-      background-color:{{$code_warna}};
-      background-size: contain;
-      background-repeat: no-repeat;
-      background-position: center;
-      height: 700px;
-      width: 100%;">
+    <div id="slider-owl" class="owl-carousel owl-theme">
+        <?php $code_warna = ''; ?>
+        @foreach(Helper::slider() as $key=>$row)
+        <?php 
+        if($row->code_warna != null){ 
+          $code_warna = $row->code_warna; 
+        }else{ 
+          $code_warna = "#dedede;";
+        }?>
+        <div class="item" data-hash="{{$key+1}}">
+            <img src="{{ $row->getFirstMediaUrl('slider') }}" alt="">
+        </div>
+        @endforeach
+    </div>
+    <div class="btn-container">
+        @foreach(Helper::slider() as $key=>$row)
+        <a class="btn-nav-slider" style="width: 75%;" href="#{{$key+1}}">{{$key+1}}</a>
+        @endforeach
+    </div>
 </div>
-</div>
-@endforeach
-</div>
-<div class="btn-container">
-  @foreach(Helper::slider() as $key=>$row)
-  <a class="btn-nav-slider" style="width: 75%;" href="#{{$key+1}}">{{$key+1}}</a>
-  @endforeach
-</div>
-</div>
-
