1fccda8a192c8195dc9aaaaf2aca20b75ff51a8f
Agus Setiawan <luciversetiawan110@gmail.com>
2020-12-08 16:38:45 +0700

    logo

--- resources/views/frontend/component/master.blade.php
+++ resources/views/frontend/component/master.blade.php
@@ -362,7 +362,7 @@
 
                                 <strong>Serpong</strong><br>
                                 <span>Senin - Jumat : 11:00 - 19:00 WIB</span><br>
-                                <span>Sabtu - Minggu : 10:00 - 18:00 WIB</span>
+                                <span>Sabtu - Minggu : 10:00 - 18:00 WIB</span><br>
 
                                 <strong>Tawakal</strong><br>
                                 <span>Senin – Minggu : 10:00 -17:00 WIB</span><br><br>
