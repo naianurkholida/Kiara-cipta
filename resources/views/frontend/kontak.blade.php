02f6d3ed470648ad1a49e30fa9912e280984eb70
Agus Setiawan <luciversetiawan110@gmail.com>
2021-01-12 16:39:27 +0700

    design contact dan menu

--- resources/views/frontend/kontak.blade.php
+++ resources/views/frontend/kontak.blade.php
@@ -9,33 +9,37 @@
 @endsection
 
 @section('content')
-<div class="container" style="margin-top: 0;padding-left: 80px;padding-right: 90px;">
+<div class="container" style="margin-top: 0;padding-left: 100px;padding-right: 110px;">
     <div class="section nobg nobottommargin clearfix" style="margin-top: 0;">
 
         <div class="container" id="container_luar">
             <div class="container" id="container_dalem">
             @include('component.alert.global')
                 <div class="row">
-                    <div class="col-lg-12">
+                    <div class="col-lg-12 col-lg-offset-12">
                         <h3 class="text-center">Hubungi Kami</h3>
                         <form method="POST" action="{{ route('inbox.post') }}">
                             @csrf
-                            <div class="form-group">
-                                <input type="text" class="form-control text-center" id="exampleFormControlInput1" placeholder="Nama Lengkap" name="name" style="border-radius: 10px;">
-                            </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control text-center" id="exampleFormControlInput1" placeholder="Masukan alamat email" name="email" style="border-radius: 10px;">
-                            </div>
-                            <div class="form-group">
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="inbox" style="border-radius: 10px;"></textarea>
-                            </div>
+                            <center>
+                                <div class="form-group">
+                                    <input type="text" class="form-control text-center" id="exampleFormControlInput1" placeholder="Nama Lengkap" name="name" style="border-radius: 10px; width: 600px;">
+                                </div> 
+                                <div class="form-group">
+                                    <input type="email" class="form-control text-center" id="exampleFormControlInput1" placeholder="Masukan alamat email" name="email" style="border-radius: 10px; width: 600px;">
+                                </div>
+                                <div class="form-group">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="inbox" style="border-radius: 10px; width: 600px;"></textarea>
+                                </div>
 
-                            <button type="submit" class="btn btn-primary">Kirim</button>
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-primary" style="width: 200px;">Kirim</button>
+                                </div>
+                            </center>
                         </form>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-lg-12">
+                    <div class="col-lg-12 text-center">
                         <!-- {!! $content->konten_page !!} -->
                         <h3 class="text-center">Lokasi Derma Express</h3><br>
 
