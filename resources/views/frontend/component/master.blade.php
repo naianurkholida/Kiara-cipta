0a855f778f8f1b0f90b1d3d8af7bc2876e35fc0d
Agus Setiawan <luciversetiawan110@gmail.com>
2021-01-14 15:06:53 +0700

    add meta name global site verification

--- resources/views/frontend/component/master.blade.php
+++ resources/views/frontend/component/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8" />
     <meta name="description" content="Page with empty content">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="_globalsign-domain-verification" content="faYUt9pHF1Oqq4zNeoIbmo5lX9cck5K_tdXhwpvmK-" />
 
     <!-- Stylesheets
     	============================================= -->
