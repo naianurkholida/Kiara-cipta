d87eca8287844a58dce3a0642fc5f18256a6a1bf
Agus Setiawan <luciversetiawan110@gmail.com>
2021-01-27 16:38:49 +0700

    produk detail

--- resources/views/frontend/products-detail.blade.php
+++ resources/views/frontend/products-detail.blade.php
@@ -19,7 +19,7 @@
                 <div class="container" id="container_detail">
                     <div class="row" style="margin-top: 70px;">
                         <div class="col-md-4 col-sm-12">
-                            <div class="img-home" style="margin-bottom:20px;width: 100%; height: 300px; background-color: #ffffff; background-image: url({{ asset('assets/admin/assets/media/derma_produk') }}/{{$data->image}}); background-size: contain;background-repeat: no-repeat;   background-position: center;"></div>
+                            <div class="img-home" style="margin-bottom:20px;width: 100%; height: 300px; background-color: #ffffff; background-image: url('{{ asset('assets/admin/assets/media/derma_produk/') }}/{{$data->image}}'); background-size: contain;background-repeat: no-repeat;   background-position: center;"></div>
                         </div>
                         <div class="col-md-8 col-sm-12">
                             {!! $data->getProdukLanguage->deskripsi !!}
