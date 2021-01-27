e9afb960312b222a4a1715eb2967791f528be2de
Agus Setiawan <luciversetiawan110@gmail.com>
2021-01-27 14:00:32 +0700

    slider

--- resources/views/frontend/component/layouts/slider.blade.php
+++ resources/views/frontend/component/layouts/slider.blade.php
@@ -9,7 +9,7 @@
           $code_warna = "#dedede;";
         }?>
         <div class="item" data-hash="{{$key+1}}">
-            <img src="{{ $row->getFirstMediaUrl('slider') }}" alt="Derma Express">
+            <img src="{{ asset('assets/admin/assets/media/slider') }}/{{$slide->image}}" alt="Derma Express">
             @if($row->link != null && $row->link != '-')
             <a href="{{ $row->link }}" class="btn btn-info button-slider" style="position: absolute; bottom: 50px; left: 39%; width: 300px; font-size: 25px;" target="blank">
                 {{ $row->title_button }}
