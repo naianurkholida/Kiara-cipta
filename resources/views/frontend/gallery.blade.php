a207b9015f8b7309f68168a0858a4d4dcb53208d
Taufiq Ridwan Soleh <taufiq26.ridwan@gmail.com>
2021-03-29 14:11:21 +0700

    gallery front end video

--- resources/views/frontend/gallery.blade.php
+++ resources/views/frontend/gallery.blade.php
@@ -10,6 +10,51 @@
 <meta property="og:description" content="Yuk Check Suasana dan Perawatan Derma di Sini." />
 
 <title>Derma Express - Gallery</title>
+
+<style>
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 999; /* Sit on top */
+  left: 0;
+  top: 0;
+  /* width: 100%; */
+  /* height: 100%; */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: fit-content;
+}
+#modal_content {
+  align-self: center;
+  width: 100%;
+  height: 315px;
+}
+
+/* The Close Button */
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
 @endsection
 
 @section('content')
@@ -42,19 +87,22 @@
                         @foreach($row->getGallery as $item)
                             @php
                                 if ($item->image != null) {
+                                    $type = "image";
                                     $url_asset = asset('assets/admin/assets/media/derma_gallery/') ."/". $item->image;
                                     $thumbnail = asset('assets/admin/assets/media/derma_gallery/') ."/". $item->image;
                                     $hover_text = "Open Image";
                                 } else {
-                                    $url_asset = $item->embed;
-                                    $thumbnail = "http://img.youtube.com/vi/".explode("v=",$item->embed)[1]."/mqdefault.jpg";
+                                    $type = "video";
+                                    $url_asset = "https://www.youtube.com/embed/".explode("=",$item->embed)[1];
+                                    $thumbnail = "https://img.youtube.com/vi/".explode("=",$item->embed)[1]."/mqdefault.jpg";
                                     $hover_text = "Watch Video";
                                 }
                             @endphp
 
                             <article class="portfolio-item pf-media pf-{{ str_replace(' ', '-', $row->category) }}" style="border: 1px solid rgb(101, 181, 170);">
                                 <div class="portfolio-image">
-                                    <a href="{{ $url_asset }}" data-lightbox="gallery">
+                                    <!-- <a href="{{ $url_asset }}" data-lightbox="gallery"> -->
+                                    <a onclick="open_modal('{{ $url_asset }}','{{ $type }}')">
                                         <div id="item-gallery" style ="background-image:url({{ $thumbnail }})"></div>
                                         <div class="portfolio-overlay">
                                             <div class="portfolio-desc">
@@ -74,5 +122,49 @@
     </div>
    
     <!-- #portfolio end -->
+
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <iframe id="modal_content" src=""></iframe>
+    </div>
+
+    </div>
 </div>
+@endsection
+
+@section('js')
+<script>
+    var modal = document.getElementById("myModal");
+    var modal_content_parent = document.getElementsByClassName("modal-content")[0];
+    var modal_content = document.getElementById("modal_content");
+    var span = document.getElementsByClassName("close")[0];
+
+    function open_modal(url_asset, type) {
+        if (type == "video")
+            modal_content_parent.style.width = "50%";
+        else
+            modal_content_parent.style.width = "fit-content";
+
+        modal.style.display = "block";
+        modal_content.setAttribute("src", url_asset);
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+        modal_content.setAttribute("src", "");
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            modal_content.setAttribute("src", "");
+        }
+    }
+</script>
 @endsection
