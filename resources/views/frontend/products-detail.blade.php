b6a2675de10e0c9afff788e9a90e1cf40db246a8
Taufiq Ridwan Soleh <taufiq26.ridwan@gmail.com>
2021-04-13 14:46:16 +0700

    zoom image detail produk

--- resources/views/frontend/products-detail.blade.php
+++ resources/views/frontend/products-detail.blade.php
@@ -20,6 +20,83 @@
 <meta property="article:section" content="Beauty &amp; health" />
 
 <title>{{$data->getProdukLanguage->judul}}</title>
+
+<style>
+    * {box-sizing: border-box;}
+
+    .img-magnifier-container {
+        position:relative;
+    }
+
+    .img-magnifier-glass {
+        position: absolute;
+        /* border: 3px solid #000; */
+        border-radius: 0%;
+        cursor: none;
+        /*Set the size of the magnifier glass:*/
+        width: 200px;
+        height: 200px;
+    }
+</style>
+
+<script>
+    // Image Magnifier
+    function magnify(imgID, zoom) {
+        var img, glass, w, h, bw;
+        img = document.getElementById(imgID);
+        console.log(img.width, img.height)
+        /*create magnifier glass:*/
+        glass = document.createElement("DIV");
+        glass.setAttribute("class", "img-magnifier-glass");
+        /*insert magnifier glass:*/
+        img.parentElement.insertBefore(glass, img);
+        /*set background properties for the magnifier glass:*/
+        glass.style.backgroundImage = "url('" + img.src + "')";
+        glass.style.backgroundRepeat = "no-repeat";
+        glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+        bw = 3;
+        w = glass.offsetWidth / 2;
+        h = glass.offsetHeight / 2;
+        /*execute a function when someone moves the magnifier glass over the image:*/
+        glass.addEventListener("mousemove", moveMagnifier);
+        img.addEventListener("mousemove", moveMagnifier);
+        /*and also for touch screens:*/
+        glass.addEventListener("touchmove", moveMagnifier);
+        img.addEventListener("touchmove", moveMagnifier);
+        function moveMagnifier(e) {
+            var pos, x, y;
+            /*prevent any other actions that may occur when moving over the image*/
+            e.preventDefault();
+            /*get the cursor's x and y positions:*/
+            pos = getCursorPos(e);
+            x = pos.x;
+            y = pos.y;
+            /*prevent the magnifier glass from being positioned outside the image:*/
+            if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+            if (x < w / zoom) {x = w / zoom;}
+            if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+            if (y < h / zoom) {y = h / zoom;}
+            /*set the position of the magnifier glass:*/
+            glass.style.left = (x - w) + "px";
+            glass.style.top = (y - h) + "px";
+            /*display what the magnifier glass "sees":*/
+            glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+        }
+        function getCursorPos(e) {
+            var a, x = 0, y = 0;
+            e = e || window.event;
+            /*get the x and y positions of the image:*/
+            a = img.getBoundingClientRect();
+            /*calculate the cursor's x and y coordinates, relative to the image:*/
+            x = e.pageX - a.left;
+            y = e.pageY - a.top;
+            /*consider any page scrolling:*/
+            x = x - window.pageXOffset;
+            y = y - window.pageYOffset;
+            return {x : x, y : y};
+        }
+    }
+</script>
 @endsection
 
 @section('content')
@@ -35,7 +112,9 @@
                 <div class="container" id="container_detail">
                     <div class="row" style="margin-top: 70px;">
                         <div class="col-md-4 col-sm-12">
-                            <div class="img-home" style="margin-bottom:20px;width: 100%; height: 300px; background-color: #ffffff; background-image: url('{{ asset('assets/admin/assets/media/derma_produk/') }}/{{$data->image}}'); background-size: contain;background-repeat: no-repeat;   background-position: center;"></div>
+                            <div class="img-magnifier-container">
+                                <img class="img-home" id="img-home" src="{{ asset('assets/admin/assets/media/derma_produk/') }}/{{$data->image}}" alt="{{$data->getProdukLanguage->judul}}" style="margin-bottom:20px;">
+                            </div>
                         </div>
                         <div class="col-md-8 col-sm-12">
                             {!! $data->getProdukLanguage->deskripsi !!}
@@ -50,6 +129,10 @@
 
 @section('js')
 <script>
+    getOriginImageSize();
+    setTimeout(function() {
+        magnify("img-home", 2);
+    }, 1000);
 
     if (window.matchMedia('(max-width: 425px)'))
     {
@@ -58,6 +141,16 @@
     } else {
         $( "#container_dalem" ).addClass("container");
         $( "#container_luar" ).removeClass("container-fluid").addClass("container");
+    }
+
+    function getOriginImageSize() {
+        var img = new Image();
+        img.src = "{{ asset('assets/admin/assets/media/derma_produk/') }}/{{$data->image}}";
+
+        var eleImg = document.getElementById("img-home");
+        img.onload = function() { 
+            eleImg.width = this.width;
+        }
     }
 </script>
 @endsection
