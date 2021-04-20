bb9f99506ceff9f7446482987b379d3b07fe06d5
Taufiq Ridwan Soleh <taufiq26.ridwan@gmail.com>
2021-04-20 20:19:45 +0700

    spek produk di list produk

--- resources/views/frontend/products.blade.php
+++ resources/views/frontend/products.blade.php
@@ -19,10 +19,26 @@
     </div>
     <div class="container-flex container">
         @foreach($data as $row)
-        <div class="box-dokter" style="height: 414px;">
-            <div class="img-dokter" style="background-image: url({{ asset('assets/admin/assets/media/derma_produk') }}/{{$row->image}});" alt="{{ asset('assets/admin/assets/media/derma_produk') }}/{{$row->image}}"></div>
-            <a href="{{ route('dermaster.products.show', $row->getProdukLanguage->seo) }}"><p id="dokter-name">{{ $row->getProdukLanguage->judul }}</p></a>
-            <p style="margin-bottom:10 !important;">{!! Helper::removeTags($row->getProdukLanguage->deskripsi) !!}</p>
+        <div class="item-bestseller">
+            <!-- <div class="box-dokter" style="height: 414px;"> -->
+            <div class="img-bestseller" style="height: 414px; border: 3px solid #67b6ab; text-align: center;">
+                <div class="img-dokter" style="background-image: url({{ asset('assets/admin/assets/media/derma_produk') }}/{{$row->image}});" alt="{{ asset('assets/admin/assets/media/derma_produk') }}/{{$row->image}}"></div>
+                <a href="{{ route('dermaster.products.show', $row->getProdukLanguage->seo) }}"><p id="dokter-name">{{ $row->getProdukLanguage->judul }}</p></a>
+                <p style="margin-bottom:10 !important;">{!! Helper::removeTags($row->getProdukLanguage->deskripsi) !!}</p>
+            </div>
+            <div class="overlay-bestseller" style="border: 3px solid #67b6ab;">
+                <div class="container-overlay">
+                    <div>
+                        <?php //$icon = Helper::iconProdukBestSeller($row->id); ?>
+                        @foreach($row->getSpec as $val)
+                        <img src="{{ asset('assets/admin/assets/media/derma_produk_spec') }}/{{ $val->icon }}" alt="" style="width: 50px;">
+                        <span>{{ $val->specification }}</span>
+                        <br><br>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="btn-bestseller-overlay"><a href="{{ route('dermaster.products.show', $row->getProdukLanguage->seo) }}" style="color: rgb(16, 158, 158) ; text-align:center;">{{$row->getProdukLanguage->judul}}</a></div>
+            </div>
         </div>
         @endforeach
     </div>
