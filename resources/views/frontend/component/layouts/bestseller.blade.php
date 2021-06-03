42fd657208b321e3f639a59af260de49562158fc
Agus Setiawan <luciversetiawan110@gmail.com>
2021-06-03 17:02:59 +0700

    produk slider

--- resources/views/frontend/component/layouts/bestseller.blade.php
+++ resources/views/frontend/component/layouts/bestseller.blade.php
@@ -1,12 +1,12 @@
 <div class="section topmargin nobottommargin nobottomborder" style="margin: 0 !important; background-color: #ffffff !important; ">
     <div class="container clearfix">
-       <center>
-            <h2>
-                <!-- Best Seller -->
-                Best Seller
-            </h2>
-        </center>
-    </div>
+     <center>
+        <h2>
+            <!-- Best Seller -->
+            Best Seller
+        </h2>
+    </center>
+</div>
 </div>
 <div class="container" style="background-color: #ffffff; z-index: 9 !important;">
     <div class="box-bestseller">
@@ -14,40 +14,41 @@
         <div class="item-bestseller">
             <div class="img-bestseller" style="background-image: url({{ asset('assets/admin/assets/media/derma_produk') }}/{{$row->produk->image}}); padding: 8px;" alt="{{ asset('assets/admin/assets/media/derma_produk') }}/{{$row->produk->image}}">
                 <p id="badge-product" style="
-                    border-radius: 4px; 
-                    background-color: #65b5aa; 
-                    color: white;
-                    padding: 8px;
-                    width: fit-content;">
-                    {{ $row->produk->getCategory->category }}
-                </p>
-            </div>
-            <div class="btn-bestseller"><a href="{{ route('dermaster.products.show', $row->produk->getProdukLanguage->seo) }}" style="color: white; text-align:center;">{{$row->produk->getProdukLanguage->judul}}</a></div>
-            <div class="overlay-bestseller">
-                <div class="container-overlay">
-                    <div>
-                        <?php //$icon = Helper::iconProdukBestSeller($row->produk->id); ?>
-                        @foreach($row->produk->getSpec as $val)
-                        <img src="{{ asset('assets/admin/assets/media/derma_produk_spec') }}/{{ $val->icon_light }}" alt="" style="width: 50px;">
-                        <span>{{ $val->specification }}</span>
-                        <br><br>
-                        @endforeach
-                    </div>
-                </div>
-                <div class="btn-bestseller-overlay"><a href="{{ route('dermaster.products.show', $row->produk->getProdukLanguage->seo) }}" style="color: rgb(16, 158, 158) ; text-align:center;">{{$row->produk->getProdukLanguage->judul}}</a></div>
-            </div>
+                border-radius: 4px; 
+                background-color: #65b5aa; 
+                color: white;
+                padding: 8px;
+                width: fit-content;">
+                {{ $row->produk->getCategory->category }}
+            </p>
         </div>
-        @endforeach
-
+        <div class="btn-bestseller"><a href="{{ route('dermaster.products.show', $row->produk->getProdukLanguage->seo) }}" style="color: white; text-align:center;">{{$row->produk->getProdukLanguage->judul}}</a></div>
+        <div class="overlay-bestseller">
+            <div class="container-overlay">
+                <div>
+                    <?php //$icon = Helper::iconProdukBestSeller($row->produk->id); ?>
+                    @foreach($row->produk->getSpec as $val)
+                    <img src="{{ asset('assets/admin/assets/media/derma_produk_spec') }}/{{ $val->icon_light }}" alt="" style="width: 50px;">
+                    <span>{{ $val->specification }}</span>
+                    <br><br>
+                    @endforeach
+                </div>
+            </div>
+            <div class="btn-bestseller-overlay"><a href="{{ route('dermaster.products.show', $row->produk->getProdukLanguage->seo) }}" style="color: rgb(16, 158, 158) ; text-align:center;">{{$row->produk->getProdukLanguage->judul}}</a></div>
+        </div>
     </div>
+    @endforeach
+</div>
 </div>
 
 <div id="section-articles" class="section page-section nomargin bgcolor clearfix" style="padding-top: 100px;background-color: #f9f9f9 !important;">
-    <div class="owl-carousel owl-theme" id="carousel-product" style="margin-top: 20px; margin-bottom: 20px;">
-        @foreach(Helper::produkList() as $item)
-        <div class="item">
-            <img src="{{ asset('assets/admin/assets/media/derma_produk') }}/{{ $item->banner }}" alt="">
+    <div class="container clearfix">
+        <div class="owl-carousel owl-theme" id="carousel-product" style="margin-top: 20px; margin-bottom: 20px;">
+            @foreach(Helper::produkList() as $item)
+            <div class="item">
+                <img src="{{ asset('assets/admin/assets/media/derma_produk') }}/{{ $item->banner }}" alt="">
+            </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
 </div>
