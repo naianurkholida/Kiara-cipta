2dc02c782c0ff759717c33754a0d1f76e50fc4c5
Agus Setiawan <luciversetiawan110@gmail.com>
2021-06-03 16:55:13 +0700

    produk slider

--- resources/views/frontend/component/layouts/bestseller.blade.php
+++ resources/views/frontend/component/layouts/bestseller.blade.php
@@ -38,13 +38,14 @@
             </div>
         </div>
         @endforeach
-
-        <div class="owl-carousel owl-theme" id="carousel-product" style="margin-top: 20px; margin-bottom: 20px;">
-            @foreach(Helper::produkList() as $item)
-            <div class="item">
-                <img src="{{ asset('assets/admin/assets/media/derma_produk') }}/{{ $item->banner }}" alt="">
+        <div class="entry-box-shadow" style="background-color: #ffffff;    border: 4px solid #ddd;min-height: 400px;">
+            <div class="owl-carousel owl-theme" id="carousel-product" style="margin-top: 20px; margin-bottom: 20px;">
+                @foreach(Helper::produkList() as $item)
+                <div class="item">
+                    <img src="{{ asset('assets/admin/assets/media/derma_produk') }}/{{ $item->banner }}" alt="">
+                </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
     </div>
 </div>
