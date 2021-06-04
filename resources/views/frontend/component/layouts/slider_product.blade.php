adc55840cf473b2bc993022e88957c0a0928d2b7
Agus Setiawan <luciversetiawan110@gmail.com>
2021-06-04 11:07:44 +0700

    produk slider

--- resources/views/frontend/component/layouts/slider_product.blade.php
+++ resources/views/frontend/component/layouts/slider_product.blade.php
@@ -4,9 +4,6 @@
 		<div class="owl-carousel owl-theme" id="carousel-product">
 			@foreach(Helper::produkList() as $item)
 			<div class="item">
-				<p id="badge-product" style="border-radius: 4px; background-color: #65b5aa; color: white;padding: 8px;width: fit-content;">
-					{{ $item->getCategory->category }}
-				</p>
 				<img src="{{ asset('assets/admin/assets/media/derma_produk') }}/{{ $item->banner }}" alt="{{ $item->getProdukLanguage->judul }}" style="padding: 5px; border-radius: 5%;">
 			</div>
 			@endforeach
