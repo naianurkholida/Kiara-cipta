b70abe76fff2b13fe17b274bf63624110bd8ab42
Agus Setiawan <luciversetiawan110@gmail.com>
2021-06-04 10:59:18 +0700

    slider product

--- resources/views/frontend/component/layouts/slider_product.blade.php
+++ resources/views/frontend/component/layouts/slider_product.blade.php
@@ -1,13 +1,13 @@
 <div id="section-articles" class="section page-section nomargin bgcolor clearfix" style="padding-top: 100px;background-color: #f9f9f9 !important;">
 	<div class="container clearfix">
-		
-		<div class="owl-carousel owl-theme" id="carousel-product" style="margin-top: 10px; margin-bottom: 10px;">
+
+		<div class="owl-carousel owl-theme" id="carousel-product">
 			@foreach(Helper::produkList() as $item)
 			<div class="item">
-				<img src="{{ asset('assets/admin/assets/media/derma_produk') }}/{{ $item->banner }}" alt="">
+				<img src="{{ asset('assets/admin/assets/media/derma_produk') }}/{{ $item->banner }}" alt="{{ $item->getProdukLanguage->judul }}" style="padding: 5px; border-radius: 5%;">
 			</div>
 			@endforeach
 		</div>
-		
+
 	</div>
 </div>
