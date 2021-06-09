461558d8553cc50f705370dfdd85f8fddb1ff015
Agus Setiawan <luciversetiawan110@gmail.com>
2021-06-09 10:55:22 +0700

    detail produk

--- resources/views/frontend/products-detail.blade.php
+++ resources/views/frontend/products-detail.blade.php
@@ -124,7 +124,7 @@
 <div class="section nobg nobottommargin clearfix" style="margin-top: 0;">
     <div class="container clearfix">
         <div class="heading-block center noborder" data-heading="O">
-            <h3 class="nott ls0">{{$data->getProdukLanguage->judul}}</h3>            
+            <h3 class="nott ls0">{{$data->getProdukLanguage->judul}}</h3>
         </div>
     </div>
     <div class="container" id="container_luar">
@@ -133,15 +133,16 @@
                 <div class="container" id="container_detail">
                     <div class="row" style="margin-top: 70px;">
                         <div class="col-md-4 col-sm-12">
+                            <div class="img-magnifier-container">
                                 <img class="img-home" id="img-home" width="100%" src="{{ asset('assets/admin/assets/media/derma_produk/') }}/{{$data->image}}" alt="{{$data->getProdukLanguage->judul}}" style="margin-bottom:20px;">
                             </div>
                         </div>
                         <div class="col-md-8 col-sm-12">
-                            <h5>
+                            <span>
                                 @if($data->harga != 0)
-                                Rp. {{ number_format($data->harga) }}
+                                    Rp. {{ number_format($data->harga) }}
                                 @endif
-                            </h5>
+                            </span>
                             {!! $data->getProdukLanguage->deskripsi !!}
                             <br>
 
