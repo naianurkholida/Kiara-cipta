945902dcd1b5cf4dbe350055c92fafa65c7db64b
Agus Setiawan <luciversetiawan110@gmail.com>
2021-06-15 12:45:55 +0700

    product

--- resources/views/frontend/products-detail.blade.php
+++ resources/views/frontend/products-detail.blade.php
@@ -169,10 +169,10 @@
                             <table class="detail-spec-mobile" style="background-color: #92cac3;">
                             @foreach($data->getSpec as $key => $val)
                                 <tr>
-                                    <td>
+                                    <td align="center">
                                         <img src="{{ asset('assets/admin/assets/media/derma_produk_spec') }}/{{ $val->icon_dark }}" alt="" style="width: 50px; margin: 16px 30px 22px 24px;">
                                     </td>
-                                    <td align="center">
+                                    <td align="center" style="color: white; font-size: 20px; font-family: ui-sans-serif;">
                                         <span>{{$val->specification}}</span>
                                     </td>
                                 </tr>
