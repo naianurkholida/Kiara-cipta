cca7414deea29fb649615fd571f5b1cb6c0bf62a
Agus Setiawan <luciversetiawan110@gmail.com>
2021-06-30 16:43:46 +0700

    optimasi image

--- resources/views/frontend/component/layouts/treatment.blade.php
+++ resources/views/frontend/component/layouts/treatment.blade.php
@@ -13,8 +13,8 @@
                 <div class="entry-box-shadow"
                     style="background-color: #ffffff;    border: 4px solid #ddd;min-height: 400px;">
                     @foreach($row->getMedia('treatment') as $val)
-                    <a href="{{ asset('assets/admin/assets/media/derma_treatment') }}/{{$val->image}}" data-lightbox="image">
-                        <img src="{{ asset('assets/admin/assets/media/derma_treatment') }}/{{$row->image}}" alt="{{ asset('assets/admin/assets/media/derma_treatment') }}/{{$row->image}}">
+                    <a href="{{ asset('assets/admin/assets/media/derma_treatment/500') }}/{{$val->image}}" data-lightbox="image">
+                        <img src="{{ asset('assets/admin/assets/media/derma_treatment/500') }}/{{$row->image}}" alt="{{ asset('assets/admin/assets/media/derma_treatment') }}/{{$row->image}}">
                     </a>
                     @endforeach
                     <div class="entry-meta-wrapper">
