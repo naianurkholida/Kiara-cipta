080421268f09230a58082b2b66fd05bef34cbf6c
Agus Setiawan <luciversetiawan110@gmail.com>
2021-09-09 12:56:20 +0700

    button janji di detail treatments

--- resources/views/frontend/component/layouts/item-detail-treatments.blade.php
+++ resources/views/frontend/component/layouts/item-detail-treatments.blade.php
@@ -2,7 +2,7 @@
 	<div class="container" id="container_detail">
 		<div class="row" style="margin-top: 70px;">
 			<div class="col-md-4 col-sm-12">
-				<div class="img-home" style="margin-bottom:20px;width: 100%; height: 300px; background-color: #ffffff; background-image: url('https://derma-express.com/assets/admin/assets/media/posting/{{ $data->image }}'); background-size: contain;background-repeat: no-repeat;   background-position: center;">
+				<div class="img-home" style="margin-bottom:20px;width: 100%; height: 300px; background-color: #ffffff; background-image: url('https://derma-express.com/assets/admin/assets/media/derma_treatment/500/{{ $data->image }}'); background-size: contain;background-repeat: no-repeat;   background-position: center;">
 				</div>
 
 				<a href="https://wa.me/message/NWV7N73Q5VWDD1" class="btn btn-info" style="width: 100%;" target="_blank">Buat Janji</a>
