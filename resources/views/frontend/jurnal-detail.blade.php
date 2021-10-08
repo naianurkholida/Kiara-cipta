4fb809952da94e1ffe83aaf14fc2709840dca689
Agus Setiawan <luciversetiawan110@gmail.com>
2021-10-08 13:47:45 +0700

    jurnal style

--- resources/views/frontend/jurnal-detail.blade.php
+++ resources/views/frontend/jurnal-detail.blade.php
@@ -57,7 +57,7 @@
 
                 @foreach($detailasc as $item)
                 <div class="box-jurnal">
-                    <div class="img-jurnal" style="background-image: url({{ asset('assets/admin/assets/media/posting/') }}/{{$item->image}});"></div>
+                    <div class="img-jurnal-detail" style="background-image: url({{ asset('assets/admin/assets/media/posting/') }}/{{$item->image}});"></div>
                     <a href="http://localhost:8000/treatments/show/treatment-1">
                         <p id="jurnal-name">
                             {{ $item->getPostingLanguage->judul }}
