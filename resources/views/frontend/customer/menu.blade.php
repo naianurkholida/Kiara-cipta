049dab968a3daa12b9c581ab3c9f579d957c8a34
Agus Setiawan <luciversetiawan110@gmail.com>
2021-12-13 11:17:38 +0700

    icon dashboard customer

--- resources/views/frontend/customer/menu.blade.php
+++ resources/views/frontend/customer/menu.blade.php
@@ -1,21 +1,26 @@
 <div class="row">
 	<div class="col-lg-2">
-		<i class="fa fa-home"></i>
+		<img src="{{ asset('assets/image/logo/home.png') }}">
 		<a href="{{ url('/dashboard-customer') }}">Beranda</a>
 	</div>	
 	<div class="col-lg-2">
+		<img src="{{ asset('assets/image/logo/user-2.png') }}">
 		<a href="{{ url('/customer/profile') }}">Profile</a>
 	</div>
 	<div class="col-lg-2">
+		<img src="{{ asset('assets/image/logo/contract.png') }}">
 		<a href="{{ url('/customer/history-transactions') }}">History Transactions</a>
 	</div>
 	<div class="col-lg-2">
+		<img src="{{ asset('assets/image/logo/password.png') }}">
 		<a href="{{ url('/sign/forgot') }}">Change Password</a>
 	</div>
 	<div class="col-lg-2">
+		<img src="{{ asset('assets/image/logo/target.png') }}">
 		<a href="{{ url('/customer/change-point') }}">Change Poin</a>
 	</div>
 	<div class="col-lg-2">
+		<img src="{{ asset('assets/image/logo/exit.png') }}">
 		<a href="{{ url('/sign/logout') }}">Logout</a>
 	</div>
 </div>
