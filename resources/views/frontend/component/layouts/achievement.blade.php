295eb5a88ff4444a29628422274c7f18219c6cc6
Agus Setiawan <luciversetiawan110@gmail.com>
2021-12-27 10:57:02 +0700

    dashboard donatur

--- resources/views/frontend/component/layouts/achievement.blade.php
+++ resources/views/frontend/component/layouts/achievement.blade.php
@@ -1,7 +1,7 @@
 <div id="section-articles" class="section page-section nomargin clearfix" style="padding-top: 100px; background: #ffffff;">
 	<div class="container clearfix">
 
-		<img src="http://localhost:8000/assets/admin/assets/media/slider/Slider_20210127025913.png">
+		<img src="{{ asset('assets/image/achivement.jpg') }}" style="max-width: 80%; display: block; margin-left: auto; margin-right: auto;">
 
 	</div>
 </div>
