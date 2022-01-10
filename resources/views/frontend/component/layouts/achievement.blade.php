038595c46d5de7ed2c8131febd4d5a47947c8a22
Agus Setiawan <luciversetiawan110@gmail.com>
2022-01-10 10:29:22 +0700

    derma setup dinamis icon

--- resources/views/frontend/component/layouts/achievement.blade.php
+++ resources/views/frontend/component/layouts/achievement.blade.php
@@ -1,7 +1,7 @@
 <div id="section-articles" class="section page-section nomargin clearfix" style="padding-top: 100px; background: #ffffff;">
 	<div class="container clearfix">
 
-		<img src="{{ asset('assets/image/achivement.jpg') }}" style="max-width: 80%; display: block; margin-left: auto; margin-right: auto;">
+		<img src="{{ asset('assets/image/award.jpg') }}" style="max-width: 80%; display: block; margin-left: auto; margin-right: auto;">
 
 	</div>
 </div>
