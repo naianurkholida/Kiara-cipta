5d1803a03ed4c1ce16ae81994c6a7bb56c7ec90d
Agus Setiawan <luciversetiawan110@gmail.com>
2022-01-24 08:58:26 +0700

    membership card

--- resources/views/frontend/auth/dashboard.blade.php
+++ resources/views/frontend/auth/dashboard.blade.php
@@ -62,30 +62,30 @@
 			<div class="col-lg-12">
 				<div class="container clearfix">
 					<div class="heading-block center noborder" data-heading="O">
-						<h3 class="nott ls0">MEMBERSHIP LEVEL</h3>Ù
+						<h3 class="nott ls0">MEMBERSHIP LEVEL</h3>
 						<span>Jadilah Member dan Dapatkan Keuntungannya</span>
 					</div>
 				</div>
 				
-					@foreach(Helper::membership() as $key => $item)
-							@if(($key%2 == 0))
-								<div class="benefit-item">
-									<img src="https://staging.derma-express.com/assets/admin/assets/media/img/turqoise_61e8edfd226d8.jpg" class="img-benefit">
-									<div class="card-body">
-										<h5 class="benefit-title">Turqoise</h5>
-										<p class="card-text">Pada level ini, transaksi anda berarti 0 sampai 30 juta</p>
-									</div>
-								</div>
-							@else
-								<div class="benefit-item benefit-rtl">
-									<img src="https://staging.derma-express.com/assets/admin/assets/media/img/turqoise_61e8edfd226d8.jpg" class="img-benefit">
-									<div class="card-body">
-										<h5 class="benefit-title">Turqoise</h5>
-										<p class="card-text">Pada level ini, transaksi anda berarti 0 sampai 30 juta</p>
-									</div>
-								</div>
-							@endif
-					@endforeach
+				@foreach(Helper::membership() as $key => $item)
+					@if(($key%2 == 0))
+						<div class="benefit-item">
+							<img src="{{ asset('assets/admin/assets/media/img') }}/{{ $item->icon }}" class="img-benefit">
+							<div class="card-body">
+								<h5 class="benefit-title">{{ $item->name }}</h5>
+								<p class="card-text">{{ $item->description }}</p>
+							</div>
+						</div>
+					@else
+						<div class="benefit-item benefit-rtl">
+							<img src="{{ asset('assets/admin/assets/media/img') }}/{{ $item->icon }}" class="img-benefit">
+							<div class="card-body">
+								<h5 class="benefit-title">{{ $item->name }}</h5>
+								<p class="card-text">{{ $item->description }}</p>
+							</div>
+						</div>
+					@endif
+				@endforeach
 
 			</div>
 		</div>
