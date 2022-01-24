91e78a39e5fe3f0ae4da05b3b9fd86e2fcf15558
Agus Setiawan <luciversetiawan110@gmail.com>
2022-01-24 14:15:34 +0700

    menu

--- resources/views/frontend/customer/menu.blade.php
+++ resources/views/frontend/customer/menu.blade.php
@@ -2,7 +2,7 @@
 	
 	@foreach(Helper::icon_menu() as $item)
 	<a href="{{ url($item->url) }}">
-		<div class="item-menu grow" style="width: 180px;">
+		<div class="item-menu grow">
 			<img style="margin-right: 10px;" src="{{ asset('assets/admin/assets/media/img') }}/{{ $item->icon }}">
 			{{ $item->name }}
 		</div>
