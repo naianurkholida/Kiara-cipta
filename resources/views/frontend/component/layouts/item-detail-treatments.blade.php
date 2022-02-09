91d1da878258b77746eff2d3791f11c0313c7f68
Agus Setiawan <retina@Retinas-MacBook-Pro.local>
2022-02-09 16:29:46 +0700

    treatments

--- resources/views/frontend/component/layouts/item-detail-treatments.blade.php
+++ resources/views/frontend/component/layouts/item-detail-treatments.blade.php
@@ -1,8 +1,9 @@
 <div class="section topmargin nobottommargin nobottomborder" style="margin: 0 !important; background-color: #ffffff !important; padding-top: 0 !important">
-	<div class="container" id="container_detail">
+	<div class="container" id="container_detail" style="padding: 20px;">
 		<div class="row" style="margin-top: 20px;">
 			<div class="col-md-6 col-sm-12">
-				<div class="img-home" style="margin-bottom:20px;width: 100%; height: 300px; background-color: #ffffff; background-image: url('https://derma-express.com/assets/admin/assets/media/derma_treatment/500/{{ $data->image }}'); background-size: contain;background-repeat: no-repeat;   background-position: center;">
+				<div class="img-home">
+					<img src="https://derma-express.com/assets/admin/assets/media/derma_treatment/500/{{ $data->image }}" style="margin-bottom:20px;width: 100%; background-color: #ffffff; background-size: contain;background-repeat: no-repeat; background-position: center; border-radius: 10px;">
 				</div>
 			</div>
 			<div class="col-md-6 col-sm-12">
