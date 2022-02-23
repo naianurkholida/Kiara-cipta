7def06573a564c48cb68d126d8a30760f29d240a
Agus Setiawan <retina@Retinas-MacBook-Pro.local>
2022-02-23 11:34:12 +0700

    breadcrumb

--- resources/views/frontend/jurnal-detail.blade.php
+++ resources/views/frontend/jurnal-detail.blade.php
@@ -45,7 +45,7 @@
             <h3 class="nott ls0">{{ $data->getPostingLanguage->judul }}</h3>
         </div>
         <div class="container-detail-jurnal">
-            <center><img src="https://derma-express.com/assets/admin/assets/media/posting/{{ $data->image }}" alt=""
+            <center><img src="https://derma-express.com/assets/admin/assets/media/posting/{{ $data->image }}" alt="{{ $data->getPostingLanguage->judul }}"
                     class="img-header-jurnal"></center>
             <div class="row" style="margin-top: 70px;">
                 <div class="col-12">
