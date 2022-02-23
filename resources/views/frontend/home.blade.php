d87b29b608f316e2fccd07675a13ec43696357eb
Agus Setiawan <retina@Retinas-MacBook-Pro.local>
2022-02-23 11:44:27 +0700

    breadcrumb

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,41 +1,46 @@
 @extends('frontend.component.master')
 
 @section('header')
-<link rel="canonical" href="https://derma-express.com">
+<title>Derma Express Klinik Kecantikan</title>
 <meta name="description" content="Klinik kecantikan dengan dokter dan layanan estetika terbaik di Indonesia.">
 <meta name="facebook-domain-verification" content="7gok7ljicai2ven06osijpt0ide58a" />
-<title>Derma Express</title>
+<meta property="og:type" content="article" />
+<meta property="og:title" content="Teaching Factory || SMKN 2 CIMAHI" />
+<meta property="og:image" content="https://derma-express.com/assets/images/LogoDermaExpress-1@4x.png" />
+<meta property="og:description"
+    content="Klinik kecantikan dengan dokter dan layanan estetika terbaik di Indonesia." />
+<meta property="og:url" content="https://derma-express.com" />
+<link rel="canonical" href="https://derma-express.com">
 
 <style type="text/css">
-.modal-dialog {
-    width: 80% !important;
-    height: 50% !important;
-    margin: 0;
-    padding: 0;
-    max-width: 80% !important;
-    padding: 0px !important;
-    margin-top: 10% !important;
-}
+    .modal-dialog {
+        width: 80% !important;
+        height: 50% !important;
+        margin: 0;
+        padding: 0;
+        max-width: 80% !important;
+        padding: 0px !important;
+        margin-top: 10% !important;
+    }
 
-.modal-content {
-    height: auto;
-    min-height: 50% !important;
-    border-radius: 0;
-    margin-left: 15%;
-    margin-right: 15%;
-}
+    .modal-content {
+        height: auto;
+        min-height: 50% !important;
+        border-radius: 0;
+        margin-left: 15%;
+        margin-right: 15%;
+    }
 
-.img-award {
-    max-width: 35%; 
-    display: block; 
-    margin-left: auto; 
-    margin-right: auto; 
-    padding-bottom: 20px;
-}
+    .img-award {
+        max-width: 35%;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        padding-bottom: 20px;
+    }
 
 
-@media (max-width: 576px){
-    .modal-show {
+    <blade media|%20(max-width%3A%20576px)%7B>.modal-show {
         padding: 0px !important;
     }
 
@@ -63,14 +68,15 @@
     }
 
     .img-award {
-        max-width: 50%; 
-        display: block; 
-        margin-left: auto; 
-        margin-right: auto; 
+        max-width: 50%;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
         padding-bottom: 20px;
     }
 
-}
+    }
+
 </style>
 @endsection
 
@@ -101,24 +107,30 @@
             <div class="modal-body">
                 <div class="row">
                     <div class="col-lg-6">
-                        <img src="{{ asset('assets/admin/assets/media/posting/') }}/{{Helper::info_desc()->image}}" style="width: 100%; height: 380px;" class="img-set">
+                        <img src="{{ asset('assets/admin/assets/media/posting/') }}/{{ Helper::info_desc()->image }}"
+                            style="width: 100%; height: 380px;" class="img-set">
                     </div>
 
                     <div class="col-lg-6">
                         @if(session()->has('message'))
-                        <div class="alert alert-success">
-                            {{ session()->get('message') }}
-                        </div>
+                            <div class="alert alert-success">
+                                {{ session()->get('message') }}
+                            </div>
                         @endif
                         <h2 class="text-center" style="margin: 30px;">{{ Helper::info_desc()->judul }}</h2>
-                        <div class="text-set" style="text-align: center; margin: 30px;"> {!! Helper::info_desc()->content !!} </div>
+                        <div class="text-set" style="text-align: center; margin: 30px;"> {!!
+                            Helper::info_desc()->content !!} </div>
 
-                        <form action="{{ url('share/update/iklan/diskon/post') }}" method="POST">
-                            {{csrf_field()}}
+                        <form action="{{ url('share/update/iklan/diskon/post') }}"
+                            method="POST">
+                            {{ csrf_field() }}
                             <div class="input-group mb-3" style="margin-top: 20px; height: 50px;">
-                                <input type="email" name="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon2" style="height: 50px; margin-left: 30px;" required="" id="email" value="">
+                                <input type="email" name="email" class="form-control" placeholder="Email"
+                                    aria-label="Email" aria-describedby="basic-addon2"
+                                    style="height: 50px; margin-left: 30px;" required="" id="email" value="">
                                 <div class="input-group-append">
-                                    <button class="btn btn-outline-success" type="submit" style="margin-right: 30px;">Send</button>
+                                    <button class="btn btn-outline-success" type="submit"
+                                        style="margin-right: 30px;">Send</button>
                                 </div>
                             </div>
                         </form>
@@ -133,10 +145,11 @@
 
 @section('js')
 <script type="text/javascript">
-    $(document).ready(function() {
-        setTimeout(function(){ 
+    $(document).ready(function () {
+        setTimeout(function () {
             $('#iklan').modal('show')
         }, 3000);
     });
+
 </script>
 @endsection
