b5e63e04c6f7dfa51dc5a848395d1f5e8066a317
Agus Setiawan <retina@Retinas-MacBook-Pro.local>
2022-05-11 11:46:19 +0700

    unsatisfied

--- app/Http/Controllers/FrontPage/HomeController.php
+++ app/Http/Controllers/FrontPage/HomeController.php
@@ -72,21 +72,7 @@ class HomeController extends Controller
     
             $res2 = $customer->getBody();
             $response_users = json_decode($res2);
-
-            $ch = curl_init();
-
-            curl_setopt($ch, CURLOPT_URL,"http://103.11.135.246:1507");
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS,"tel=081274587066");
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $server_output = curl_exec($ch);
             
-            curl_close ($ch);
-            
-
-            dd($server_output);
-
             $msg = '';
             $msg_error = '';
             return view('frontend.unsatisfied', compact('trx_no','msg','msg_error'));
@@ -98,15 +84,17 @@ class HomeController extends Controller
             if ($file) {
                 $fileName = 'Reason'.'_'.uniqid().'.'.$file->getClientOriginalExtension();
                 Image::make($file)->save($this->path.'/'. $fileName);
-                $client = new Client();
-                $response = $client->request('POST', 'http://103.11.135.246:1507/Unsatisfied?no_trx='.str_replace(',','', $trx_no).'&image=https://derma-express.com/'.$this->path.'/'.$fileName.'&reason='.$reason);
+                // $client = new Client();
+                // $response = $client->request('POST', 'http://103.11.135.246:1507/Unsatisfied?no_trx='.str_replace(',','', $trx_no).'&image=https://derma-express.com/'.$this->path.'/'.$fileName.'&reason='.$reason);
         
-                $res = $response->getBody();
+                // $res = $response->getBody();
                 // $data = json_decode($res);
                 
-                $msg = 'Pesan anda berhasil terkirim, Terimakasih DexPeople';
-                $msg_error = '';
-                return view('frontend.unsatisfied', compact('trx_no','msg','msg_error'));
+                // $msg = 'Pesan anda berhasil terkirim, Terimakasih DexPeople';
+                // $msg_error = '';
+                // return view('frontend.unsatisfied', compact('trx_no','msg','msg_error'));
+
+                return redirect('http://103.11.135.246:2310/apex/f?p=887:4:9448631956690:::4:P4_TRX_NO,P4_REASON,P4_LINK_IMG:'.str_replace(',','', $trx_no).',https://derma-express.com/'.$this->path.'/'.$fileName.','.$reason);
             }else{
                 $msg = '';
                 $msg_error = 'Pesan anda gagal terkirim';
