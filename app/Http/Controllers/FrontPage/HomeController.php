eb6895d6fb80f03fbea25cb9b436c8d7cc4a9396
Agus Setiawan <retina@Retinas-MacBook-Pro.local>
2022-05-11 11:26:20 +0700

    unsatisfied

--- app/Http/Controllers/FrontPage/HomeController.php
+++ app/Http/Controllers/FrontPage/HomeController.php
@@ -73,7 +73,19 @@ class HomeController extends Controller
             $res2 = $customer->getBody();
             $response_users = json_decode($res2);
 
-            dd($response_users);
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL,"http://103.11.135.246:1507");
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS,"tel=081274587066");
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $server_output = curl_exec($ch);
+            
+            curl_close ($ch);
+            
+
+            dd($server_output);
 
             $msg = '';
             $msg_error = '';
